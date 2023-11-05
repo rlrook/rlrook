e404268ad094d5a13094b457678b8b4c6d498667
rlrook <144726241+rlrook@users.noreply.github.com>
2023-11-05 08:15:47 -0600

    Add files via upload

---
+++ docs/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,12 @@
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"What",p:["p1"]},{w:"is",p:["p1"]},{w:"Duo?",p:["p1"]},{w:"Device",p:["p2","p5"]},{w:"Requirements",p:["p2"]},{w:"for",p:["p2","p4"]},{w:"Duo",p:["p2","p3","p4"]},{w:"How",p:["p3","p4"]},{w:"to",p:["p3","p4"]},{w:"Add",p:["p3"]},{w:"Devices",p:["p3"]},{w:"Enable",p:["p4"]},{w:"the",p:["p4"]},{w:"First",p:["p4"]},{w:"Time",p:["p4"]},{w:"Reactivate",p:["p5"]},{w:"A",p:["p5"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3, 4, 5];
+ph["p2"]=[6, 7, 8, 9];
+ph["p3"]=[10, 11, 12, 13, 11, 9];
+ph["p4"]=[10, 11, 14, 9, 8, 15, 16, 17];
+ph["p5"]=[18, 19, 6];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
---
+++ docs/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,2 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+var linkToParent = {3:-1,2:3,1:-1,5:3,4:-1};
